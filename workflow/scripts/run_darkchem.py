92ff83ac8e705e249ae19b1dbbb04c01d6826042
christinehc <christine.chang@pnnl.gov>
2025-10-28 11:00:52 -0700

    fix: separately load models (instead of using dict)

--- workflow/scripts/run_darkchem.py
+++ workflow/scripts/run_darkchem.py
@@ -18,9 +18,6 @@ MODEL_PATH = {
 # DATA_PATH = "./data/test_batch_size-10.tsv"  # some tsv file
 DATA_PATH = snakemake.input[0]
 
-# load models
-model = {k: darkchem.utils.load_model(MODEL_PATH[k]) for k in MODEL_PATH.keys()}
-
 # load data
 data = pd.read_csv(DATA_PATH, sep="\t", header=0)
 data = data[data["adduct"].isin(snakemake.config["adducts"])]  # restrict adducts
@@ -28,16 +25,19 @@ data = data[data["adduct"].isin(snakemake.config["adducts"])]  # restrict adduct
 predicted_ccs = list()
 for adduct in snakemake.config["adducts"]:
     tmp = data[data["adduct"] == adduct]
+
+    # load models
+    model = darkchem.utils.load_model(MODEL_PATH[adduct])
     x = np.array(
         [darkchem.utils.struct2vec(smi) for smi in tmp["smi"]]
     )  # extract only SMILES notation
     # essentially what this does is that in a for loop, it will go over each SMILES notation and turn it into a numerical encoding of it and create a table where each row is a SMILES notation
 
     # generate latent space
-    x_latent = model[adduct].encoder.predict(x)
+    x_latent = model.encoder.predict(x)
 
     # generate property predictions
-    y_pred = model[adduct].predictor.predict(x_latent)
+    y_pred = model.predictor.predict(x_latent)
 
     predict_df = pd.DataFrame({"adductID": tmp["adduct_id"], "darkchem": y_pred[:, 1]})
     predicted_ccs.append(predict_df)
